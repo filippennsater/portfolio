fd6e887c871ff8f2ac6a4205cd8fa64358862aeb
filippennsater <filip.pennsater@hotmail.com>
2023-03-28 00:56:59 +0200

    fixed buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
+
+
+const STYLES = ['btn--primary', 'btn--outline']
+
+
+export const Button = ({ children, type, onClick, buttonStyle, buttonSize }) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle)
+};
