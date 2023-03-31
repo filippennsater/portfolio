09e978c649129f5c57bae5cd43571f4de6e76c69
filippennsater <filip.pennsater@hotmail.com>
2023-04-01 00:26:17 +0200

    started fixing carditems

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,11 +7,11 @@ function HeroSection() {
     return (
         <div className='hero-container'>
             <video src='../..//videos/video-1.mp4' autoPlay loop muted />
-            <h1>ADVENTURE AWAITS</h1>
-            <p>What are you waiting for?</p>
+            <h1>WELCOME TO MY PORTFOLIO!</h1>
+            <p>So... who am I?</p>
             <div className='hero-btns'>
                 <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
-                    GET STARTED
+                    Debscription
                 </Button>
 
                 <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>
