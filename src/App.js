cbe4a8fcf859506af6f6c23c0b700ba52b4f7dd5
filippennsater <filip.pennsater@hotmail.com>
2023-03-31 22:24:44 +0200

    fixed some styling for herocontainer and a new cardclass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import './App.css';
+import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/Home'
+
 
 function App() {
   return (
@@ -8,7 +11,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact />
+          <Route path="/" exact Component={Home} />
         </Routes>
       </Router>
 
