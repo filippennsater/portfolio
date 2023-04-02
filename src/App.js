14903f3f4829f48ed0c1c236531ec262aeabbf03
filippennsater <filip.pennsater@hotmail.com>
2023-04-02 21:26:07 +0200

    fixed routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './components/pages/Home'
+
+import Home from './components/pages/Home';
+import Projects from './components/pages/Projects';
+import CV from './components/pages/Cv';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import SignUp from './components/pages/SignUp';
 
 
 function App() {
@@ -12,6 +18,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" exact Component={Home} />
+          <Route path="/projects" exact Component={Projects} />
+          <Route path="/cv" exact Component={CV} />
+          <Route path="/about" exact Component={About} />
+          <Route path="/contact" exact Component={Contact} />
+          <Route path="/sign-up" exact Component={SignUp} />
         </Routes>
       </Router>
 
