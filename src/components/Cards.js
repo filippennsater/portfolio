b82ab065a4aebe91f6b0e4b8249cf212f0152fef
filippennsater <filip.pennsater@hotmail.com>
2023-04-02 03:14:47 +0200

    finished cards and added images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import CardItem from './CardItem';
+import './Cards.css';
 
 function Cards () {
     return (
         <div className='cards'>
-            <h1>check it out wowowowo!</h1>
+            <h1>Some of my projects:</h1>
             <div className='cards__container'>
                 <div className='cards__wrapper'>
-                    
+
+                    <ul className='cards__items'>
+                        <CardItem src='images/img-8ball.png' text='Magic 8-ball' label='Small project' path='/projects' />
+                        <CardItem src='images/img-Apiano.png' text='The Amazing Piano' label='Small project' path='/projects' />
+                    </ul>
+
+                    <ul className='cards__items'>
+                        <CardItem src='images/img-3.jpg' text='Cool image huh?' label='Bigger Project' path='/projects' />
+                        <CardItem src='images/img-4.jpg' text='Cool image huh?' label='Bigger Project' path='/projects' />
+                    </ul>
+
                 </div>
-            </div>
-            
+            </div>           
         </div>
     )
 }
