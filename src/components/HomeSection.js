882b51f0cbd9863797f06b2c800cef8ce838ee46
filippennsater <filip.pennsater@hotmail.com>
2023-04-30 11:35:02 +0200

    fixed video class that enables styling it was not working before but just set to the standard "video"-class default

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <video src='../..//videos/video-1.mp4' autoPlay loop muted />
+            <video className='vid' src='../..//videos/video-1.mp4' autoPlay loop muted />
 
             <div className='selfie-container'>
                 <figure class="home-self" />
