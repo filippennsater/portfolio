9ac09c089fd8b5651fd43b225dea3469428beaad
filippennsater <filip.pennsater@hotmail.com>
2023-05-07 19:11:53 +0200

    Fixed so darkmode is rendered when button is clicked and you don't need to rerender page.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,15 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CardItem from './CardItem';
 import './Cards.css';
+import { createContext } from 'react';
+
+export const ThemeContext = createContext(null);
 
 function Cards () {
 
     var theme = localStorage.getItem("theme");
 
+    const[theS, setTheS] = useState(localStorage.getItem("theme"));
+
+
+
+    const toggleBoggle = () => {
+        setTheS(theme);
+    };
+
+
+    let changed = false;
+
+    function yourFunction(){
+
+        var wasss = theme;
+
+        var isss = localStorage.getItem("theme");
+
+        changed = !(wasss === isss);        //if they are the same "changed" should be false
+
+        theme = localStorage.getItem("theme");
+    
+        setTimeout(yourFunction, 10);       //this makes sure that the function keeps checking if the user pressed the darkmode-button
+
+
+        if (changed){
+
+            toggleBoggle();                 //need to call toggleBoggle aka rerenderingfunction in if statement, else React will refuse because it's and infinite re-render
+            changed = false;
+
+        }
+        
+    }
+    
+    yourFunction();
+
+   
+
 
 
     return (
-        <div className={`cards-${theme}`}>
+        <ThemeContext.Provider value={{theS, toggleBoggle}}>
+        <div className='cards' id={theS}>
+            
             <h1><br />Some of my projects <i className="fas fa-laptop-code"></i></h1>
             <div className='cards__container'>
                 <div className='cards__wrapper'>
@@ -28,7 +70,8 @@ function Cards () {
                 </div>
             </div>           
         </div>
-    )
+        </ThemeContext.Provider>
+    );
 }
 
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -32,6 +32,7 @@ function Navbar() {
         }
 
         localStorage.setItem('theme', DMTheme);
+
     };
 
 
