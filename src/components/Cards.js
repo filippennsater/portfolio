a9ce29974b5c6e4ec76051dfc7d67dac26df80ef
filippennsater <filip.pennsater@hotmail.com>
2023-10-03 00:31:09 +0200

    added a new project in home and projects

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -63,7 +63,7 @@ function Cards () {
                     </ul>
 
                     <ul className='cards__items'>
-                        <CardItem src='images/img-8ball.png' text='Magic 8-ball' label='Small project' path='https://magic8ball-50df8.web.app/' />
+                        <CardItem src='images/img-DesignCase1.png' text='Designcase 1' label='Small project' path='https://designcase1-6b885.web.app/' />
                         <CardItem src='images/img-Apiano.png' text='The Amazing Piano' label='Small project' path='https://theamazingpiano-65a1c.web.app/' />
                     </ul>
 
--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -56,9 +56,9 @@ function ProjectSection() {
 
 
 
+
                     <ul className='cards__items'>
                             <CardItem src='images/img-portfolio1.png' text='My Portfolio' label='Bigger Project' path='/' />
-
 
                             <div className='information'>
                                 <h4>My Portfolio</h4>
@@ -69,8 +69,7 @@ function ProjectSection() {
                                 </ul>
 
                                 <div className='info_figures'>
-
-                                    
+                          
                                     <div className='figures'><i className="fa-brands fa-react"></i></div>
                                     
                                     <div className='figures'><i className="fa-brands fa-html5"></i></div>
@@ -80,20 +79,16 @@ function ProjectSection() {
                                     <div className='figures'><i className="fa-brands fa-sass"></i></div>
 
                                     <div className='figures'><i className="fa-brands fa-git"></i></div>
-
-                                    
+                       
                                 </div>
-
                             </div>
-
-
                         </ul>
+
 
 
 
                         <ul className='cards__items'>
                             <CardItem src='images/img-GoodGains.png' text='GoodGains' label='Bigger Project' path='https://goodgains-1202d.web.app' />
-
 
                             <div className='information'>
                                 <h4>GoodGains</h4>
@@ -104,8 +99,7 @@ function ProjectSection() {
                                 </ul>
 
                                 <div className='info_figures'>
-
-                                    
+                                  
                                     <div className='figures'><i className="fa-brands fa-react"></i></div>
                                     
                                     <div className='figures'><i className="fa-brands fa-html5"></i></div>
@@ -115,49 +109,46 @@ function ProjectSection() {
                                     <div className='figures'><i className="fa-brands fa-sass"></i></div>
 
                                     <div className='figures'><i className="fa-brands fa-git"></i></div>
-
-                                    
+                                
                                 </div>
-
                             </div>
-
-
                         </ul>
 
 
 
-                        <ul className='cards__items'>
-                            <CardItem src='images/img-8ball.png' text='Magic 8-ball' label='Small project' path='https://magic8ball-50df8.web.app/' />
 
+                        <ul className='cards__items'>
+                            <CardItem src='images/img-DesignCase1.png' text='Designcase 1' label='Small project' path='https://designcase1-6b885.web.app/' />
 
                             <div className='information'>
-                                <h4>Magic 8-ball</h4>
+                                <h4>Designcase 1</h4>
                                 <ul>
-                                    <ptext><br /><br />&emsp;A small website that generate random answers<br /></ptext>
-                                    <ptext><br />&emsp;Focus on usage of styling and javascripts<br /></ptext>
+                                    <ptext><br /><br />&emsp;A simple designcase<br /></ptext>
+                                    <ptext><br />&emsp;Focus on styling and using React<br /></ptext>
                                     <ptext><br />&emsp;Hosted through google-firebase <i className="fa-solid fa-fire"></i><br /></ptext>
                                 </ul>
 
                                 <div className='info_figures'>
-
+                                 
+                                    <div className='figures'><i className="fa-brands fa-react"></i></div>
+                                    
                                     <div className='figures'><i className="fa-brands fa-html5"></i></div>
 
                                     <div className='figures'><i className='fa-brands fa-square-js'></i></div>
 
-                                    <div className='figures'><i className="fa-brands fa-css3"></i></div>
+                                    <div className='figures'><i className="fa-brands fa-sass"></i></div>
 
                                     <div className='figures'><i className="fa-brands fa-git"></i></div>
-
-
+          
                                 </div>
-
                             </div>
-
                         </ul>
+
+
+
 
                         <ul className='cards__items'>
                             <CardItem src='images/img-Apiano.png' text='The Amazing Piano' label='Small project' path='https://theamazingpiano-65a1c.web.app/' />
-
 
                             <div className='information'>
                                 <h4>The Amazing Piano</h4>
@@ -178,10 +169,34 @@ function ProjectSection() {
                                     <div className='figures'><i className="fa-brands fa-git"></i></div>
 
                                 </div>
-
                             </div>
+                        </ul>
 
 
+
+
+                        <ul className='cards__items'>
+                            <CardItem src='images/img-8ball.png' text='Magic 8-ball' label='Small project' path='https://magic8ball-50df8.web.app/' />
+                            <div className='information'>
+                                <h4>Magic 8-ball</h4>
+                                <ul>
+                                    <ptext><br /><br />&emsp;A small website that generate random answers<br /></ptext>
+                                    <ptext><br />&emsp;Focus on usage of styling and javascripts<br /></ptext>
+                                    <ptext><br />&emsp;Hosted through google-firebase <i className="fa-solid fa-fire"></i><br /></ptext>
+                                </ul>
+
+                                <div className='info_figures'>
+
+                                    <div className='figures'><i className="fa-brands fa-html5"></i></div>
+
+                                    <div className='figures'><i className='fa-brands fa-square-js'></i></div>
+
+                                    <div className='figures'><i className="fa-brands fa-css3"></i></div>
+
+                                    <div className='figures'><i className="fa-brands fa-git"></i></div>
+
+                                </div>
+                            </div>
                         </ul>
 
 
